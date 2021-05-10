3ca3867c64142b3a14276798ee0d37c421d13906
nanasuzi <kamizyou3@gmail.com>
2021-05-10 13:01:06 +0000

    [Finish] 4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ce840720e8f8e82fb75a2f393f011fbb30fdbc30b68ec9185d9d8bb9d45caa6ed34f2aa71cfd0d7586e43ff8ee8dda25add3fa2a05a56c0e5f9a23e86af18b95'
